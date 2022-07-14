1446c86b4e553f51d5a06b1283958e15d1ca438d
Thuotracy <tracyjacobs775@gmail.com>
2022-07-14 17:42:45 +0300

    updated the main.js file

---
+++ main.js
@@ -0,0 +1,18 @@
+let btn = document.getElementById('btn');
+let output = document.getElementById('outputtext');
+
+let number = [Math.floor(Math.random() * 100)]
+
+btn.addEventListener('click', function(){
+    let input = document.getElementById('userInput').value;
+    if(input == number){
+        output.innerHTML = `You guessed right, your number was ${number}`
+    }
+        else if (input < number){
+            output.innerHTML = "you guessed too low!"
+        };
+
+        if (input > number){
+            output.innerHTML = "Yor guessed too high!"
+        }
+})
